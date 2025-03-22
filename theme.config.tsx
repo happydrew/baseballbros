2eee828362a9d36b95d9853d88f5b47cfbb054c5
zhugechina <zhugetd@126.com>
2025-03-22 22:03:32 +0800

    add more games

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,8 +11,8 @@ const inter = Inter({ subsets: ['latin'] });
 const logo = (
   <div className="flex justify-center items-center" title="AutoFormAI">
     <Image src="/favicon.png" alt="baseballbros logo" width={40} height={40} />
-    <span className={`${inter.className} font-bold font-cartoon ml-2 text-2xl mt-1 text-yellow-500`}>
-      BaseballBrosIO
+    <span className={`${inter.className} font-cartoon ml-2 text-cyan-400 text-4xl font-black drop-shadow-[0_0_10px_#00FFFF]`}>
+      Baseball<span className='text-red-500'>Bros</span>IO
     </span>
   </div>
 )
