0d52ef59cc317ccef8635b0b34512a643665b648
zhugechina <zhugetd@126.com>
2025-03-27 22:58:01 +0800

    add google ads

--- components/games/GameCard.tsx
+++ components/games/GameCard.tsx
@@ -14,7 +14,12 @@ const GameCard = ({
             className="relative w-[7rem] h-[7rem] bg-center bg-cover rounded-lg transform transition duration-300 hover:scale-110"
         >
             <div className="absolute flex justify-center items-center w-full bottom-0 bg-gradient-to-t rounded-lg to-transparent">
-                <span className="leading-4 w-full text-sm text-center mb-1 text-white font-cartoon font-bold break-words drop-shadow-[2px_2px_0px_black]">{name}</span>
+                <span
+                    className="leading-4 w-full text-sm text-center mb-1 text-white font-cartoon font-bold break-words"
+                    style={{ textShadow: "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000" }}
+                >
+                    {name}
+                </span>
             </div>
         </a>
     )
